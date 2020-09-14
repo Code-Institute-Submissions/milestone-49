f9c1cab0ca5df11e79d94e585a3b8a3423ae2f2f
umujtaba-ci <umujtaba@canadianbusinesscollege.com>
2020-09-14 08:25:55 +0000

    Added search and sorting functionality

--- books/views.py
+++ books/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Book, Category
 
 def all_books(request):
     """ A view to return all books """
 
     books = Book.objects.all()
+    query = None
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            books = books.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a keyword or ISBN to search.")
+                return redirect(reverse('books'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(description_full__icontains=query) | Q(isbn10__icontains=query) | Q(isbn13__icontains=query) | Q(category__friendly_name__icontains=query) 
+
+            books = books.filter(queries)
 
     context = {
         'books': books,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'books/books.html', context)
