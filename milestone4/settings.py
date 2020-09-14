910d2d52f855bf6bc05b0112618e55be8705be75
umujtaba-ci <umujtaba@canadianbusinesscollege.com>
2020-09-14 01:51:34 +0000

    Removed template files. Updated base.html.

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -123,10 +123,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
