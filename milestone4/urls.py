250c2ae3d1d3c38691f9c90d5600745087b43d3f
umujtaba-ci <umujtaba@canadianbusinesscollege.com>
2020-09-15 03:39:19 +0000

    Added views, urls, and static folder for payment.

--- milestone4/urls.py
+++ milestone4/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('books', include('books.urls')),
     path('cart', include('cart.urls')),
+    path('payment', include('payment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
